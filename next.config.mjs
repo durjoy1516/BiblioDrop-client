10f00a20f0d16f98029c8f8d9d1224b52ea93919
durjoy1516 <durjoy0184@gmail.com>
2026-08-12 03:11:07 +0600

    backend part added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co", // ImgBB এর ছবির জন্য
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // Google Auth অবতারের জন্য
+      },
+    ],
+  },
 };
 
 export default nextConfig;
